083c02c424300352c6391c059a449ba9f2a3896b
ChadPro <1193041759@qq.com>
2017-10-19 11:58:51 +0800

    Create Project
    Create Project

---
+++ QSCWImageProcessing/QSCWImageProcessing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QSCWImageProcessing
+//
+//  Created by candela on 2017/10/19.
+//  Copyright © 2017年 Qscw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QSCWImageProcessing/QSCWImageProcessing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QSCWImageProcessing
+//
+//  Created by candela on 2017/10/19.
+//  Copyright © 2017年 Qscw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
